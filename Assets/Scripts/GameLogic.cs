718b8b8c0310bf261b8b73303d40ebf9065f6ee9
Quintuplize <quintuplize@gmail.com>
2025-07-13 16:25:58 -0400

    finished pause menu

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -7,13 +7,14 @@ public class GameLogic : MonoBehaviour
     public int playerScore;
     public Text playerScoreText;
     public GameObject gameOverScreen;
+    public GameObject pauseMenu;
     public bool gameIsActive = true;
 
     [ContextMenu("Increase Score")]
     public void IncrementPlayerScore()
     {
         if (gameIsActive)
-        { 
+        {
             playerScore++;
             playerScoreText.text = playerScore.ToString();
         }
@@ -26,12 +27,49 @@ public class GameLogic : MonoBehaviour
 
     public void GameOver()
     {
-        gameOverScreen.SetActive(true);
         gameIsActive = false;
+        ApplyTimeScale();
+        gameOverScreen.SetActive(true);
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene("MainGameplayScene");        
-    } 
+        SceneManager.LoadScene("MainGameplayScene");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    void TogglePause()
+    {
+        gameIsActive = !gameIsActive;
+        ApplyPauseState();
+    }
+
+    public void ResumeGame()
+    {
+        gameIsActive = true;
+        ApplyPauseState();
+    }
+
+    void ApplyPauseState()
+    {
+        ApplyTimeScale();
+        pauseMenu.SetActive(!gameIsActive);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void ApplyTimeScale()
+    { 
+        Time.timeScale = gameIsActive ? 1 : 0;
+    }
 }
